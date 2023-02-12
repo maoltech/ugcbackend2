dfb8e2282c9b5632382dbf9ec7f4dbd155b058ae
hengage <tunecarterkillz@gmail.com>
2023-02-12 08:35:51 +0100

    Feature: user model

---
+++ server/model/users.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+
+class User extends Model {}
+User.init({
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'user'
+});
+
+module.exports = User;
