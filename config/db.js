cd21d919360e4dbaa785ce9525dd90e2fa0ec54b
maoltech <hardeyshinnah.mo@gmail.com>
2023-02-14 12:52:19 -0500

    online db

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const ConnectDb = {
     process.env.DB_NAME, 
     process.env.DB_USERNAME, 
     process.env.DB_PASSWORD, {
-    host: process.env.DB_LOCALHOST,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     pool: {
       max: 5,
